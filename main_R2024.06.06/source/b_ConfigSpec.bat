87019324224e8087e72a251576534d3b19213de2
Raajnaag HULIYAPURADA MATA <nagaraja.hm.0001@gmail.com>
2024-06-08 17:18:09 +0530

    Initial status

--- main_R2024.06.06/source/b_ConfigSpec.bat
+++ main_R2024.06.06/source/b_ConfigSpec.bat
@@ -10,6 +10,7 @@ MKLINK /D McalGpt         ..\..\..\..\repo_submodules\McalGpt\VirtualMachinev01_
 MKLINK /D McalLin         ..\..\..\..\repo_submodules\McalLin\VirtualMachinev01_Hypervisorv01_R2024.01.24
 MKLINK /D McalMcu         ..\..\..\..\repo_submodules\McalMcu\VirtualMachinev01_Hypervisorv01_R2023.10.18
 MKLINK /D McalWdg         ..\..\..\..\repo_submodules\McalWdg\VirtualMachinev01_Hypervisorv01_R2023.09.21
+MKLINK /D ProjectARA      ..\..\..\..\repo_submodules\ProjectARA\VirtualMachinev01_Hypervisorv01_R2024.01.24
 MKLINK /D ProjectCfg      ..\..\..\..\repo_submodules\ProjectCfg\VirtualMachinev01_Hypervisorv01_R2024.06.06
 MKLINK /D ProjectMemMap   ..\..\..\..\repo_submodules\ProjectMemMap\VirtualMachinev01_Hypervisorv01_R2024.01.24
 MKLINK /D SwcApplDcm      ..\..\..\..\repo_submodules\SwcApplDcm\VirtualMachinev01_Hypervisorv01_R2023.10.18
@@ -20,15 +21,15 @@ MKLINK /D SwcServiceBswM  ..\..\..\..\repo_submodules\SwcServiceBswM\VirtualMach
 MKLINK /D SwcServiceCom   ..\..\..\..\repo_submodules\SwcServiceCom\VirtualMachinev01_Hypervisorv01_R2023.10.18
 MKLINK /D SwcServiceComM  ..\..\..\..\repo_submodules\SwcServiceComM\VirtualMachinev01_Hypervisorv01_R2023.10.18
 MKLINK /D SwcServiceDcm   ..\..\..\..\repo_submodules\SwcServiceDcm\VirtualMachinev01_Hypervisorv01_R2023.10.18
-MKLINK /D SwcServiceDem   ..\..\..\..\repo_submodules\SwcServiceDem\VirtualMachinev01_Hypervisorv01_R2023.10.18
+MKLINK /D SwcServiceDem   ..\..\..\..\repo_submodules\SwcServiceDem\VirtualMachinev01_Hypervisorv01_R2024.06.06
 MKLINK /D SwcServiceDet   ..\..\..\..\repo_submodules\SwcServiceDet\VirtualMachinev01_Hypervisorv01_R2023.09.21
-MKLINK /D SwcServiceEcuM  ..\..\..\..\repo_submodules\SwcServiceEcuM\VirtualMachinev01_Hypervisorv01_R2023.10.18
+MKLINK /D SwcServiceEcuM  ..\..\..\..\repo_submodules\SwcServiceEcuM\VirtualMachinev01_Hypervisorv01_R2024.06.06
 MKLINK /D SwcServiceFiM   ..\..\..\..\repo_submodules\SwcServiceFiM\VirtualMachinev01_Hypervisorv01_R2023.09.21
 MKLINK /D SwcServiceNvM   ..\..\..\..\repo_submodules\SwcServiceNvM\VirtualMachinev01_Hypervisorv01_R2023.10.18
 MKLINK /D SwcServiceOs    ..\..\..\..\repo_submodules\SwcServiceOs\VirtualMachinev01_Hypervisorv01_R2024.01.24
 MKLINK /D SwcServicePduR  ..\..\..\..\repo_submodules\SwcServicePduR\VirtualMachinev01_Hypervisorv01_R2023.10.18
 MKLINK /D SwcServiceSchM  ..\..\..\..\repo_submodules\SwcServiceSchM\VirtualMachinev01_Hypervisorv01_R2023.09.21
 MKLINK /D SwcServiceSmCan ..\..\..\..\repo_submodules\SwcServiceSmCan\VirtualMachinev01_Hypervisorv01_R2023.10.18
-MKLINK /D SwcServiceTpCan ..\..\..\..\repo_submodules\SwcServiceTpCan\VirtualMachinev01_Hypervisorv01_R2024.03.07
+MKLINK /D SwcServiceTpCan ..\..\..\..\repo_submodules\SwcServiceTpCan\VirtualMachinev01_Hypervisorv01_R2024.06.06
 MKLINK /D common          ..\..\..\..\repo_submodules\common\VirtualMachinev01_Hypervisorv01_R2023.10.18
 
